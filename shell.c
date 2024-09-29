92ea7aa881e6123aebefb98450b40f1293aeece2
Pranay-Vaka <vakapranay05@gmail.com>
2024-09-29 23:54:51 +0100

    finished the move instruction section with all functions

--- shell.c
+++ shell.c
@@ -26,6 +26,7 @@ typedef struct State8080 {
     uint8_t int_enable;
 } State8080;
 
+
 // this is for any instruction that we have not yet implemented, so that the program does not crash
 void UnimplementedInstruction(State8080* state) {
     // the pc will increment by 1, so we will decrement now to compenstate for that
@@ -34,8 +35,15 @@ void UnimplementedInstruction(State8080* state) {
     exit(1);
 }
 
-// FLAGS -- Defines all the flags using bitshifts
+// FLAGS -- Constant flags made from bitshifts to manipulate different flags
 
+// bitshifts each of the flags so that it can be logically OR'd to make it 1
+#define Z_FLAG (1<<7)
+#define S_FLAG (1<<6)
+#define P_FLAG (1<<5)
+#define CY_FLAG (1<<4)
+#define AC_FLAG (1<<3)
+#define FLAGS (Z_FLAG | S_FLAG | P_FLAG | CY_FLAG | AC_FLAG) // used to set all the flags as true
 
 // CHECK FLAGS -- Checks certain results for the flags
 
@@ -88,17 +96,17 @@ uint16_t makeWord(uint8_t a, uint8_t b) {
 }
 
 // makes the values in register h and l into a word
-uint16_t makeWordhl(State8080 *state) {
+uint16_t makeWordHL(State8080 *state) {
     return makeWord(state->h, state->l);
 }
 
 // makes the values in register b and c into a word
-uint16_t makeWordbc(State8080 *state) {
+uint16_t makeWordBC(State8080 *state) {
     return makeWord(state->b, state->c);
 }
 
 // makes the values in register d and e into a word
-uint16_t makeWordde(State8080 *state) {
+uint16_t makeWordDE(State8080 *state) {
     return makeWord(state->d, state->e);
 }
 
@@ -112,18 +120,38 @@ void breakWord(uint8_t *a, uint8_t *b, uint16_t word) {
 }
 
 // breaks the word into two bytes stored in h and l respectively
-void breakWordhl(State8080 *state, uint16_t word) {
+void breakWordHL(State8080 *state, uint16_t word) {
     breakWord(&state->h, &state->l,  word);
 }
 
 // breaks the word into two bytes stored in b and c respectively
-void breakWordbc(State8080 *state, uint16_t word) {
+void breakWordBC(State8080 *state, uint16_t word) {
     breakWord(&state->b, &state->c,  word);
 }
 
 // breaks the word into two bytes stored in d and e respectively
-void breakWordde(State8080 *state, uint16_t word) {
+void breakWordDE(State8080 *state, uint16_t word) {
     breakWord(&state->d, &state->e,  word);
+}
+
+// returns the byte at a certain index in the memory of the state machine
+uint8_t readByte(State8080* state, uint16_t index) {
+    return state -> memory[index];
+}
+
+// inserts byte into a certain index in the memory array
+void writeByte(State8080* state, uint16_t index, uint8_t value) {
+    state->memory[index] = value;
+}
+
+// getters and setters for the registers H and L
+uint16_t getHL(State8080* state) {
+    return readByte(state, makeWordHL(state));
+}
+
+void setHL(State8080* state, uint8_t value) {
+    uint16_t index = makeWordHL(state);
+    writeByte(state, index, value);
 }
 
 
@@ -168,7 +196,7 @@ void Emulate8080p(State8080* state) {
             state->b = state->l;
             break;
         case 0x46:
-            state->b = get_hl_mem(state);
+            state->b = getHL(state);
             break;
         case 0x47:
             state->b = state->a;
@@ -192,7 +220,7 @@ void Emulate8080p(State8080* state) {
             state->c = state->l;
             break;
         case 0x4e:
-            state->c = get_hl_mem(state);
+            state->c = getHL(state);
             break;
         case 0x4f:
             state->c = state->a;
@@ -216,7 +244,7 @@ void Emulate8080p(State8080* state) {
             state->d = state->l;
             break;
         case 0x56:
-            state->d = get_hl_mem(state);
+            state->d = getHL(state);
             break;
         case 0x57:
             state->d = state->a;
@@ -240,7 +268,7 @@ void Emulate8080p(State8080* state) {
             state->e = state->l;
             break;
         case 0x5e:
-            state->e = get_hl_mem(state);
+            state->e = getHL(state);
             break;
         case 0x5f:
             state->e = state->a;
@@ -264,7 +292,7 @@ void Emulate8080p(State8080* state) {
             state->h = state->l;
             break;
         case 0x66:
-            state->h = get_hl_mem(state);
+            state->h = getHL(state);
             break;
         case 0x67:
             state->h = state->a;
@@ -288,35 +316,35 @@ void Emulate8080p(State8080* state) {
             state->l = state->l;
             break;
         case 0x6e:
-            state->l = get_hl_mem(state);
+            state->l = getHL(state);
             break;
         case 0x6f:
             state->l = state->a;
             break;
         case 0x70:
-            set_hl_mem(state, state->b);
+            setHL(state, state->b);
             break;
         case 0x71:
-            set_hl_mem(state, state->c);
+            setHL(state, state->c);
             break;
         case 0x72:
-            set_hl_mem(state, state->d);
+            setHL(state, state->d);
             break;
         case 0x73:
-            set_hl_mem(state, state->e);
+            setHL(state, state->e);
             break;
         case 0x74:
-            set_hl_mem(state, state->h);
+            setHL(state, state->h);
             break;
         case 0x75:
-            set_hl_mem(state, state->l);
+            setHL(state, state->l);
             break;
         case 0x76:
-            printf("Execution has been halted\n");
+            printf("Halting emulation");
             exit(0);
             break;
         case 0x77:
-            set_hl_mem(state, state->a);
+            setHL(state, state->a);
             break;
         case 0x78:
             state->a = state->b;
@@ -337,7 +365,7 @@ void Emulate8080p(State8080* state) {
             state->a = state->l;
             break;
         case 0x7e:
-            state->a = get_hl_mem(state);
+            state->a = getHL(state);
             break;
         case 0x7f:
             state->a = state->a;
